18dad22198a922f674feda8b03745e78e310bf88
Bartłomiej Trzonek <barthek7@gmail.com>
2018-05-23 20:53:44 +0200

    Add reset button and progress bar to carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 var flkty = new Flickity('.main-carousel', {
-   cellAlign: 'left',
-   contain: true,
    pageDots: false,
    hash: true
+});
+
+var rstBtn = document.getElementById('reset-btn');
+rstBtn.addEventListener('click', function (){
+   flkty.select(0, false, false);
+});
+var progressBar = document.querySelector('.progress-bar')
+flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
 });
